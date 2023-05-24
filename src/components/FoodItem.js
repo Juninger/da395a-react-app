19531ffa159a2bc75e1c05d2be935574afe16d14
Juninger <marcus.juninger@gmail.com>
2023-05-25 01:26:02 +0200

    First working modal draft
    - When a FoodItem is clicked, additional data is fetched from the API to populate modal
    - Currently styling of the modal makes no sense

--- src/components/FoodItem.js
+++ src/components/FoodItem.js
@@ -2,30 +2,46 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 import SaveButton from './SaveButton';
 import DeleteButton from './DeleteButton';
-import Button from 'react-bootstrap/Button'
 import { useState } from 'react';
 import FoodItemModal from './FoodItemModal';
+import axios from 'axios';
 
 
-export default function FoodItem({item, saveButton, activateModal}) {
-  const [modalShow, setModalShow] = useState(false);
-  const title = item.strMeal;
-  const description = item.strInstructions;
-  const thumbnail = item.strMealThumb;
+export default function FoodItem({ meal, saveButton }) {
+  const [showModal, setShowModal] = useState(false);
+  const [mealDetails, setMealDetails] = useState(null);
+
+  function handleItemClick() {
+    const url = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=';
+    const id = meal.idMeal;
+
+    axios.get(url + id) //fetches specific meal with more details
+      .then((response) => {
+        console.log(response.data.meals[0]);
+        setMealDetails(response.data.meals[0]);
+        setShowModal(true);
+      });
+  };
+
+  function hideModal() {
+    setShowModal(false);
+  };
 
   return (
     <>
-      <Card>
-        <Card.Body>
-          <Card.Title>{title}</Card.Title>
-          <Card.Body>{description}</Card.Body>
-          <Card.Img src={thumbnail}></Card.Img>
-          {saveButton ? <SaveButton/> : <DeleteButton/>} 
-          <Button variant='secondary' onClick={() => setModalShow(true)}>Show recipe</Button>
-        </Card.Body>
-      </Card>
-      <FoodItemModal show={modalShow} data={item} onHide={() => setModalShow(false)}></FoodItemModal>
+      <div onClick={handleItemClick}>
+        <Card >
+          <Card.Body>
+            <Card.Title>{meal.strMeal}</Card.Title>
+            <Card.Body>{meal.strInstructions}</Card.Body>
+            <Card.Img src={meal.strMealThumb}></Card.Img>
+            {saveButton ? <SaveButton /> : <DeleteButton />}
+          </Card.Body>
+        </Card>
+      </div>
+      {mealDetails && (
+        <FoodItemModal show={showModal} onHide={hideModal} meal={mealDetails}></FoodItemModal>
+      )}
     </>
   )
-
 }
--- src/components/FoodItemModal.js
+++ src/components/FoodItemModal.js
@@ -2,37 +2,52 @@ import React from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
-import Image from 'react-bootstrap/Image';
 
 
-export default function FoodItemModal({show, data, onHide}) {
+export default function FoodItemModal({ show, meal, onHide }) {
+
+  const YOUTUBE_ID = extractVideoID(meal.strYoutube);
+
+  function extractVideoID(url) { //also known as: 'not writing regex for this'
+    try {
+      const youtubeURL = new URL(url);
+      const params = new URLSearchParams(youtubeURL.search);
+      return params.get('v');
+    } catch (error) { //no video in response or invalid URL
+      return null;
+    }
+  }
+
   return (
-    <Modal show={show} className='modal-xl'>
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          {data.strMeal}
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body className="show-grid">
-        <Container>
-          <Row>
-            <Col xs={6} md={6}>
-              <Image src={data.strMealThumb} fluid='true'/>
-            </Col>
-            <Col xs={6} md={6}>
-              <iframe class='responsive-iframe' src={data.strYoutube} height='75%' width='100%' title="YouTube video player"></iframe>
-            </Col>
-          </Row>
 
-          <Row>
-              {data.strInstructions}
-          </Row>
-        </Container>
+    <Modal show={show} onHide={onHide}>
+      <Modal.Header closeButton>
+        <Modal.Title>{meal.strMeal}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Row>
+          <Col xs={6} md={6}>
+            <img src={meal.strMealThumb} alt={meal.strMeal} />
+          </Col>
+          <Col xs={6} md={6}>
+            {YOUTUBE_ID && <iframe //only show embed if we have a valid video-id
+              width="100%"
+              height="315"
+              src={"https://www.youtube.com/embed/" + YOUTUBE_ID}
+              title="YouTube Video"
+              allowFullScreen
+            ></iframe>}
+          </Col>
+        </Row>
+        <Row>
+          <p>{meal.strInstructions}</p>
+        </Row>
       </Modal.Body>
       <Modal.Footer>
-        <Button onClick={onHide}>Close</Button>
+        <Button variant="secondary" onClick={onHide}>
+          Close
+        </Button>
       </Modal.Footer>
     </Modal>
   )
