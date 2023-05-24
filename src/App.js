d54b424d63bca0519fa0f5729d49536ebd4c61d2
Juninger <marcus.juninger@gmail.com>
2023-05-24 19:38:17 +0200

    Expanded select-options and started recipe search
    - Now calls API twice to fetch Categories + Areas, then builds Form.Select with all options.
    - Started functionality to call API with selected value to find recipes

--- src/App.js
+++ src/App.js
@@ -4,24 +4,57 @@ import Container from 'react-bootstrap/Container';
 import SearchFilter from './components/SearchFilter';
 import FoodList from './components/FoodList';
 import MyRecipesInfo from './components/MyRecipesInfo';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 
 function App() {
 
+  // const firstRender = useRef(true); //SHOULD be used instead of renderCount, temporary fix for strict-mode double rendering
+  // const renderCount = useRef(0);
+
   const baseURL = 'https://www.themealdb.com/api/json/v1/1/';
   const [categories, setCategories] = useState([]);
+  const [areas, setAreas] = useState([]);
+  // const [selectedSearch, setSelectedSearch] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
 
   useEffect(() => {
-    axios.get(baseURL + 'list.php?c=list')
-    .then((response) => {
-      console.log(response.data);
+    axios.get(baseURL + 'list.php?c=list') //fetches list of categories
+      .then((response) => {
+        setCategories(response.data.meals);
+      });
 
-      setCategories(response.data.meals);
-    });
-
-    //TODO: fetch more areas for filter/search?
+    axios.get(baseURL + 'list.php?a=list') //fetches list of areas
+      .then((response) => {
+        setAreas(response.data.meals);
+      });
   }, []);
+
+  // Called when user changes selected value in the list of categories / areas (in <SearchFilter>)
+  function getRecipes(event) {
+    const newSelectedOption = event.target.value;
+    
+    // setSelectedSearch(newSelectedOption);
+  }
+
+  
+  // useEffect(() => {
+  //   /* SHOULD be used instead of renderCount solution below, temporary fix for strict-mode double rendering
+  //   if (firstRender.current) {
+  //     firstRender.current = false;
+  //   } else {
+  //     console.log("new value of selsearch:", selectedSearch);
+  //   }
+  //   */
+
+  //   renderCount.current += 1;
+
+  //   if (renderCount.current > 2) {
+  //     console.log("Calling the API with", selectedSearch);
+  //   }
+
+  // }, [selectedSearch]);
+  
 
 
   return (
@@ -30,7 +63,7 @@ function App() {
       <Row>
         <Col md={12} xl={6}>
           <div>
-            <SearchFilter categories={categories}></SearchFilter>
+            <SearchFilter categories={categories} areas={areas} selectChange={getRecipes}></SearchFilter>
             <FoodList></FoodList>
           </div>
         </Col>
--- src/components/SearchFilter.js
+++ src/components/SearchFilter.js
@@ -1,16 +1,27 @@
 import React from 'react'
 import Form from 'react-bootstrap/Form';
 
-export default function SearchFilter({ categories }) {
+export default function SearchFilter({ categories, areas, selectChange }) {
   return (
     <>
-      <h5>Categories</h5>
-      <Form.Select aria-label="Default select example">
-        <option disabled>CATEGORIES</option>
-        {categories.map(category =>
-          <option key={category.strCategory}>{category.strCategory}</option>
-        )}
-      </Form.Select>
+      <Form.Group>
+        <Form.Label>Categories</Form.Label>
+        <Form.Select defaultValue={'Select an option'} onChange={selectChange}>
+          <option disabled hidden>Select an option</option>
+          <optgroup label="Categories">
+            {categories.map(category =>
+              <option value={category.strCategory} label={category.strCategory} key={category.strCategory}></option>
+            )}
+          </optgroup>
+
+          <optgroup label="Areas">
+            {areas.map(area =>
+              <option value={area.strArea} label={area.strArea} key={area.strArea}></option>
+            )}
+          </optgroup>
+        </Form.Select>
+      </Form.Group>
+
     </>
   )
 }
