5ecbd236cadd69785723cdb6bf8cd08a21c04ccb
Juninger <marcus.juninger@gmail.com>
2023-05-24 16:25:46 +0200

    Populating category-select
    Uses axios to fetch all available categories and builds <SearchFilter> component with response

--- src/App.js
+++ src/App.js
@@ -4,16 +4,29 @@ import Container from 'react-bootstrap/Container';
 import SearchFilter from './components/SearchFilter';
 import FoodList from './components/FoodList';
 import MyRecipesInfo from './components/MyRecipesInfo';
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const baseURL = 'https://www.themealdb.com/api/json/v1/1/';
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    axios.get(baseURL + 'categories.php')
+    .then((response) => {
+      setCategories(response.data.categories);
+    });
+  }, []);
+
+
   return (
     <Container>
       <h1>Recipe App</h1>
       <Row>
         <Col md={12} xl={6}>
           <div>
-            <SearchFilter></SearchFilter>
+            <SearchFilter categories={categories}></SearchFilter>
             <FoodList></FoodList>
           </div>
         </Col>
--- src/components/SearchFilter.js
+++ src/components/SearchFilter.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import Form from 'react-bootstrap/Form';
 
-export default function SearchFilter() {
+export default function SearchFilter({ categories }) {
   return (
-    <div>SearchFilter</div>
+    <>
+      <h5>Categories</h5>
+      <Form.Select aria-label="Default select example">
+        {categories.map(category =>
+          <option key={category.idCategory}>{category.strCategory}</option>
+        )}
+      </Form.Select>
+    </>
   )
 }
